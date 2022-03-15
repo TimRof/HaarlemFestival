9dd39a3e5097e9aa79d736867a11c7627e470571
Jelle de Vries (student github) <70753141+H4B1TZ@users.noreply.github.com>
2022-03-15 17:38:05 +0100

    initial commit
    started on index, css and cartscript

--- app/views/purchase/index.php
+++ app/views/purchase/index.php
@@ -1,3 +1,18 @@
-<h1><a href="/">The Haarlem Festival</a></h1>
-
-<h3>Purchase</h3>
+<?php
+session_start()
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Haarlem Festival Cart</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+	<meta name="author" content="Jelle de Vries"> 
+    <link href="css/Main.css" rel="stylesheet" type="text/css">
+    <script src="js/CartScript.js"></script>
+</head>
+<body>
+	<h3> work in progress <h3>
+</body>
+</html>
